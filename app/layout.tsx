cbca684b2f140cdef3d5a20e1eb0eb62dc1a58e6
Stan Liu <stanleyliu@berkeley.edu>
2023-09-05 13:18:44 -0700

    Update favicon image to svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.svg" />
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-324NEVCL3L" />
       <Script id="google-analytics">
         {`
